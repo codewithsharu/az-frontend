d8eaa0e3eb861df648c657a5986c8fe7da1f30f3
Dandasi <Shareen.Dandasi@kroll.com>
2026-04-09 14:56:27 +0530

    basic react page

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [status, setStatus] = useState("Checking...");
+  const [lastUpdated, setLastUpdated] = useState("-");
+  const [data, setData] = useState([]);
+
+ const fetchStatus = async () => {
+  try {
+    const res = await fetch("http://localhost:5000/api/status");
+    const result = await res.json();
+
+    const formattedDate = new Date(result.lastUpdated).toLocaleString(
+      "en-US",
+      {
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "2-digit",
+        hour12: true
+      }
+    );
+
+    setStatus(result.backendStatus);
+    setLastUpdated(formattedDate);
+  } catch (err) {
+    setStatus("DOWN");
+    setLastUpdated("-");
+  }
+};
+
+
+  const fetchData = async () => {
+    const res = await fetch("http://localhost:5000/api/employees");
+    const result = await res.json();
+    setData(result);
+  };
+
+  useEffect(() => {
+    fetchStatus();
+  }, []);
+
+  return (
+    <div className="container">
+      <h2>CI/CD PRACTICE DASHBOARD</h2>
+
+      <div className="status-box">
+        <p><strong>Backend Status:</strong> {status}</p>
+        <p><strong>Last Updated:</strong> {lastUpdated}</p>
+      </div>
+
+      <button onClick={fetchData}>Fetch Data</button>
+
+      {data.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Employee ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((emp) => (
+              <tr key={emp.id}>
+                <td>{emp.id}</td>
+                <td>{emp.name}</td>
+                <td>{emp.empId}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default App;
